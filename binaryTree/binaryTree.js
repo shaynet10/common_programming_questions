3127a1bf1e0e52eadc2a562324ec02af8ad86aaa
Shay Gueta <shaynet10@gmail.com>
2021-05-18 23:16:10 +0300

    Binary search tree support lca and path to value

--- binaryTree/binaryTree.js
+++ binaryTree/binaryTree.js
@@ -16,6 +16,62 @@ class BinaryTree {
         console.log(JSON.stringify(node, null, 4));
     }
 
+    getAllNodes(node = this.root, nodesArray = []) {
+        nodesArray.push(node);
+        if (node.left) {
+            this.getAllNodes(node.left, nodesArray);
+        }
+        if (node.right) {
+            this.getAllNodes(node.right, nodesArray);
+        }
+        return nodesArray;
+    }
+
+    leftView(node = this.root, direction = "left") {
+        if (direction === "left") { 
+            console.log(node.value);
+        }
+        if (node.right) {
+            this.leftView(node.right, "right");
+        }
+        if (node.left) {
+            this.leftView(node.left, "left");
+        }
+    }
+
+    getPathToValue(value, options = "") {
+        let path = [];
+        let current = this.root;
+        while (current) {
+            path.push(current);
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                if (options === "valuesOnly") {
+                    return path.map((node) => node.value);
+                }
+                return path;
+            }
+        }
+        return [];
+    }
+
+
+    lowestCommonAncestor(value1, value2, options = "") {
+        const path1 = this.getPathToValue(value1);
+        const path2 = this.getPathToValue(value2);
+        const path2Set = new Set(path2);
+        for (let path1Index = path1.length - 1; path1Index >= 0; path1Index--) {
+            const path1Node = path1[path1Index];
+            if (path2Set.has(path1Node)) {
+                if (options === "valuesOnly") return path1Node.value;
+                return path1Node;
+            }
+        }
+    }
+
     isLeaf(node) {
         return node && !node.left && !node.right;
     }
--- binaryTree/index.js
+++ binaryTree/index.js
@@ -10,3 +10,32 @@ binaryTree.addValue(7);
 binaryTree.addValue(13);
 
 binaryTree.printTree();
+
+console.log("Left view of tree is");
+binaryTree.leftView();
+
+let pathToValue, lca;
+console.log("path to value 7");
+pathToValue = binaryTree.getPathToValue(7, "valuesOnly");
+console.log(pathToValue);
+
+console.log("path to value 14");
+pathToValue = binaryTree.getPathToValue(14, "valuesOnly");
+console.log(pathToValue);
+
+console.log("Least common anseccor of 7 and 14");
+lca = binaryTree.lowestCommonAncestor(7, 14, "valuesOnly");
+console.log(lca);
+
+
+console.log("path to value 4");
+pathToValue = binaryTree.getPathToValue(4, "valuesOnly");
+console.log(pathToValue);
+
+console.log("path to value 7");
+pathToValue = binaryTree.getPathToValue(7, "valuesOnly");
+console.log(pathToValue);
+
+console.log("Least common anseccor of 4 and 7");
+lca = binaryTree.lowestCommonAncestor(4, 7, "valuesOnly");
+console.log(lca);
